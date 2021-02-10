eb68af7662d4fd8ec45061615ac7e37377068241
GrantZvolsky <grant@zvolsky.org>
2021-02-10 01:01:38 +0000

    add etc/profile[.d/*]

---
+++ etc/profile.d/dotnet-cli-tools-bin-path.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#export DOTNET_ROOT=~/dotnet
+##export DOTNET_ROOT=/snap/dotnet-sdk/current
+##export DOTNET_ROOT=/snap/dotnet-sdk/47
+##export DOTNET_ROOT=/opt/dotnet-sdk
+#export MSBuildSDKsPath=$DOTNET_ROOT/sdk/$(${DOTNET_ROOT}/dotnet --version)/Sdks
+#
+##DEFAULT_PATH=`cat /etc/environment | grep "^PATH" | sed 's/PATH=\"//' | sed 's/\"$//'`
+##export PATH="${DEFAULT_PATH}:${DOTNET_ROOT}:$HOME/.dotnet/tools"
+#
+#export PATH="${PATH}:${DOTNET_ROOT}:$HOME/.dotnet/tools"
